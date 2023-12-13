1124c1e3e795f9676126ad723079847184439f44
Krathish sk <krathishs.k123@gmail.com>
2023-12-13 12:41:43 +0530

    Completed stack navigation

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import AppNavigation from "./navigation/AppNavigation";
 
 export default function App() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
